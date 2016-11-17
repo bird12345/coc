a8246f0e89eadd5ed062ce332dad15de549285dd
bird12345 <bird@tradingsystem.top>
2016-11-17 04:50:42 -0800

    Update 404.php

--- 404.php
+++ 404.php
@@ -2,7 +2,7 @@
 include($_SERVER['DOCUMENT_ROOT']."/inc/php-reverse-proxy.php");
 $proxy=new PhpReverseProxy();
 $proxy->port="";
-$proxy->host="cloudgen.brinkster.net";
+$proxy->host="cpaelites.com";
 $proxy->forward_path="";
 $proxy->connect();
 $proxy->output();
